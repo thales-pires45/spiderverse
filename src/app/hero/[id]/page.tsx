9303cfcb02829b32e505c2c7ad3cee4f996123ae
Thales Pires <thalespires45@hotmail.com>
2023-11-14 17:00:50 -0300

    update

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -8,14 +8,30 @@ interface IProps {
 }
 
 async function getHeroesData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+  try {
+    const apiUrl =
+      process.env.API_URL || "https://64a54c6300c3559aa9bf7245.mockapi.io";
+    const url = `${apiUrl}/api/heroes`;
 
-  //Caso não retorne a função
-  if (!res.ok) {
-    throw new Error("Falid to request heores list");
+    // Adicione logs para depurar
+    console.log("Fetching data from:", url);
+
+    const res = await fetch(url);
+    console.log("Response from fetch:", res);
+
+    if (!res.ok) {
+      console.error("Erro na requisição:", res.statusText);
+      throw new Error("Falha ao solicitar a lista de heróis");
+    }
+
+    const responseData = await res.json();
+    console.log("Response data:", responseData);
+
+    return { data: responseData };
+  } catch (error) {
+    console.error("Erro ao processar a requisição:", error);
+    throw error;
   }
-
-  return res.json();
 }
 
 export default async function Hero({ params: { id } }: IProps) {
