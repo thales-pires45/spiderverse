d0e0179936a4dbed59b62678d8bc084c1b9d7a36
Thales Pires <thalespires45@hotmail.com>
2023-11-14 15:19:34 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,20 @@ import styles from "./page.module.scss";
 
 //Faz uma requisição assincrona
 async function getHeroesDate(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
-  console.log("Response from fetch:", res);
+  try {
+    const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+    console.log("Response from fetch:", res);
 
-  //Caso não retorne a função
-  if (!res.ok) {
-    throw new Error("Falid to request heores list");
+    if (!res.ok) {
+      console.error("Erro na requisição:", res.statusText);
+      throw new Error("Falha ao solicitar a lista de heróis");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Erro ao processar a requisição:", error);
+    throw error;
   }
-
-  return res.json();
 }
 
 export default async function Home() {
