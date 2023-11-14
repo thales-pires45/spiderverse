e0a397a57c659e23f161c0f913a15033a3afe4b0
Thales Pires <thalespires45@hotmail.com>
2023-11-14 14:30:02 -0300

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://64a54c6300c3559aa9bf7245.mockapi.io",
-    DOMAIN_ORIGIN: "http://127.0.0.1:3000",
+    DOMAIN_ORIGIN: "https://vercel.com/thales-pires45s-projects/spiderverse",
   },
 };
 
